dc1c02b48075a41e72535999118e463a8b420742
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-19 11:46:21 +0100

    Project settings

--- src/Credfeto.DotNet.Code.Analysis.Overrides/XmlUpdater.cs
+++ src/Credfeto.DotNet.Code.Analysis.Overrides/XmlUpdater.cs
@@ -1,36 +1,37 @@
+using System;
 using System.Xml;
 
-namespace Credfeto.DotNet.Code.Analysis.Overides;
+namespace Credfeto.DotNet.Code.Analysis.Overrides;
 
 public static class XmlUpdater
 {
-    public static void ChangeValue(XmlDocument xmlDoc, string ruleSet, string rule, string name, string newState)
+    public static void ChangeValue(this XmlDocument xmlDoc, string ruleSet, string rule, string name, string newState)
     {
         // Construct the XPath query to find the Rule element
         string query = $"//RuleSet/Rules[@AnalyzerId='{ruleSet}']/Rule[@Id='{rule}']";
 
         // Find the element using XPath
-        XmlNode element = xmlDoc.SelectSingleNode(query);
+        XmlElement? element = xmlDoc.SelectSingleNode(query) as XmlElement;
 
-        if (element != null)
+        if (element is null)
         {
-            // Get the existing 'Action' attribute value
-            string existingValue = element.Attributes["Action"]?.Value;
+            Console.Error.WriteLine($"'{name}' is not present");
 
-            if (existingValue == newState)
-            {
-                Console.WriteLine($"Did not change '{name}' status as already '{newState}'");
-            }
-            else
-            {
-                // Set the new value to the 'Action' attribute
-                element.Attributes["Action"].Value = newState;
-                Console.WriteLine($"Updating '{name}' from '{existingValue}' to '{newState}'");
-            }
+            return;
+        }
+
+        // Get the existing 'Action' attribute value
+        string existingValue = element.GetAttribute("Action");
+
+        if (StringComparer.Ordinal.Equals(existingValue, newState))
+        {
+            Console.WriteLine($"Did not change '{name}' status as already '{newState}'");
         }
         else
         {
-            Console.Error.WriteLine($"'{name}' is not present");
+            // Set the new value to the 'Action' attribute
+            element.SetAttribute("Action",  newState);
+            Console.WriteLine($"Updating '{name}' from '{existingValue}' to '{newState}'");
         }
     }
 }
