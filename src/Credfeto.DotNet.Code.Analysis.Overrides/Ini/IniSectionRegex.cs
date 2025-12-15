4fbc545952946e85d9f2d0622bdf4df1bb085d6d
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-15 19:00:55 +0000

    Start of parsing Ini files

---
+++ src/Credfeto.DotNet.Code.Analysis.Overrides/Ini/IniSectionRegex.cs
@@ -0,0 +1,18 @@
+using System.Text.RegularExpressions;
+
+namespace Credfeto.DotNet.Code.Analysis.Overrides.Ini;
+
+internal static partial class IniSectionRegex
+{
+    private const int TIMEOUT_MS = 1000;
+    private const RegexOptions REGEX_OPTIONS = RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.NonBacktracking | RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture;
+
+    [GeneratedRegex(pattern: @"^\s*\[(?<Section>([^#;]|\\#|\\;)+)\]\s*([#;].*)?$", options: REGEX_OPTIONS, matchTimeoutMilliseconds: TIMEOUT_MS)]
+    public static partial Regex Section();
+
+    [GeneratedRegex(pattern: @"^\s*[#;]", options: REGEX_OPTIONS, matchTimeoutMilliseconds: TIMEOUT_MS)]
+    public static partial Regex Comment();
+
+    [GeneratedRegex(pattern: @"^\s*(?<Key>[\w\.\-_]+)\s*[=:]\s*(?<Value>.*?)\s*([#;].*)?$", options: REGEX_OPTIONS, matchTimeoutMilliseconds: TIMEOUT_MS)]
+    public static partial Regex Property();
+}
