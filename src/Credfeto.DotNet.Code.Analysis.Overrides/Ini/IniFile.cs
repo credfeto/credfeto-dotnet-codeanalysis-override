f5d7025188b7cd3084a96e664f2204aab0177200
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-12-19 11:41:02 +0000

    Simplifying

--- src/Credfeto.DotNet.Code.Analysis.Overrides/Ini/IniFile.cs
+++ src/Credfeto.DotNet.Code.Analysis.Overrides/Ini/IniFile.cs
@@ -2,64 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Credfeto.DotNet.Code.Analysis.Overrides.Ini.Helpers;
 
 namespace Credfeto.DotNet.Code.Analysis.Overrides.Ini;
 
-public sealed class IniFile
+public static class IniFile
 {
-    private IniFile(IniSection global, Dictionary<string, IniSection> namedSections)
-    {
-        this.NamedSections = namedSections;
-        this.Global = global;
-    }
-
-    private IniSection Global { get; }
-
-    private IDictionary<string, IniSection> NamedSections { get; }
-
-    public string Save()
-    {
-        StringBuilder stringBuilder = new();
-
-        bool previousSection = false;
-
-        if (!this.Global.IsEmpty)
-        {
-            stringBuilder = this.Global.Save(stringBuilder);
-            previousSection = true;
-        }
-
-        foreach ((_, string _, IniSection values) in this.NamedSections.Select(item => (order: item.Value.Order, section: item.Key, entries: item.Value))
-                                                               .OrderBy(item => item.order))
-        {
-            if (!values.IsEmpty)
-            {
-                if (previousSection)
-                {
-                    stringBuilder = stringBuilder.AppendLine();
-                }
-
-                stringBuilder = values.Save(stringBuilder);
-                previousSection = true;
-            }
-        }
-
-        return stringBuilder.ToString();
-    }
-
-    public static async ValueTask<IniFile> LoadAsync(string fileName, CancellationToken cancellationToken)
+    public static async ValueTask<ISettings> LoadAsync(string fileName, CancellationToken cancellationToken)
     {
         string[] lines = await File.ReadAllLinesAsync(path: fileName, cancellationToken: cancellationToken);
 
         return Extract(lines);
     }
 
-    private static IniFile Extract(in ReadOnlySpan<string> lines)
+    private static ISettings Extract(in ReadOnlySpan<string> lines)
     {
         int order = 0;
         IniSection globalSection = new(order: order, name: null, []);
@@ -71,8 +30,8 @@ public sealed class IniFile
 
         foreach (string line in lines)
         {
-            if (string.IsNullOrWhiteSpace(line) || Helpers.IniSectionRegex.Comment()
-                                                          .IsMatch(line))
+            if (string.IsNullOrWhiteSpace(line) || IniSectionRegex.Comment()
+                                                                  .IsMatch(line))
             {
                 commentLines.Add(line);
 
@@ -93,13 +52,13 @@ public sealed class IniFile
             commentLines = [];
         }
 
-        return new(global: globalSection, namedSections: namedSections);
+        return new Settings(global: globalSection, namedSections: namedSections);
     }
 
     private static bool IsSection(string line, [NotNullWhen(true)] out string? sectionTitle)
     {
-        Match match = Helpers.IniSectionRegex.Section()
-                             .Match(line);
+        Match match = IniSectionRegex.Section()
+                                     .Match(line);
 
         if (!match.Success)
         {
@@ -113,7 +72,7 @@ public sealed class IniFile
         return true;
     }
 
-    public static IniFile Load(string content)
+    public static ISettings Load(string content)
     {
         using (StringReader reader = new(content))
         {
